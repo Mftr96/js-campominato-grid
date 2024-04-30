75858b849a7f097889a479d30a33433bb3f7aea0
Mftr96 <matteo.federici96@gmail.com>
2024-04-30 16:24:47 +0200

    milestone 4 ( creazione di foglio js a parte, e creazione funzione nella quale si trova la creazione dell'elemento e l'event listener)

---
+++ JS/function.js
@@ -0,0 +1,29 @@
+
+function f_quadrato(i) {
+    let quadrato = document.createElement("div");
+
+    quadrato.classList.add("quadrato")
+
+    quadrato.innerText = i;
+
+    quadrato.addEventListener("click", function () {
+
+        quadrato.classList.toggle("azzurro")
+
+        console.log(i)
+        
+
+    });
+
+    return quadrato;
+}
+
+
+let griglia = document.getElementById("griglia");
+
+for (let i = 1; i <= 100; i++) {
+    let quadrato=f_quadrato(i);
+
+    griglia.append(quadrato);
+
+    }
--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,23 @@
 
 //richiamo in JS la griglia 
-let griglia=document.getElementById("griglia");
+let griglia = document.getElementById("griglia");
 
 //creo un ciclo nel quale creo i quadrati della griglia 
-for (let i = 1; i <=100; i++) {
+for (let i = 1; i <= 100; i++) {
     /*creo la variabile quadrato con all'interno il div 
     (specificare prima il document, così crei un elemento etereo nella pagina
     che potrà poi essere implementato)
     */
-    let quadrato=document.createElement("div");
+    let quadrato = document.createElement("div");
     //aggiungo la classe quadrato che dà le dimensioni
     quadrato.classList.add("quadrato")
     /*aggiungo il numero del quadrato come testo dentro al div,
     ossia il numero del ciclo*/
-    quadrato.innerText=i;
+    quadrato.innerText = i;
 
     /*dico al quadrato di agire quando viene cliccato, ovvero 
     gli dico di cambiare colore e fare console.log */
-    quadrato.addEventListener("click",function(){
+    quadrato.addEventListener("click", function () {
         // cambio colore
         quadrato.classList.toggle("azzurro")
         //scrivo in console il valore del quadrato con il valore attuale
@@ -25,4 +25,6 @@ for (let i = 1; i <=100; i++) {
     })
     //inserisco il div appena creato nella griglia
     griglia.append(quadrato);
-}
+    }
+
+    
